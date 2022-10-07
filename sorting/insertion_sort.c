1532ed824667e70508bdcd709792cb91d297d3e8
JamiroMR <bgutierrezJY@hotmail.com>
2022-10-07 10:17:19 -0500

    bubble sorting added

--- sorting/insertion_sort.c
+++ sorting/insertion_sort.c
@@ -12,7 +12,14 @@ void insertionSort(int a[], int n){
 	}
 }
 void bubble_sort(int a[], int n){
-
+	for(int i=1; i<n; i++)
+		for(int j=0; j<n-1; j++){
+			if(a[j]>a[j+1]){
+				int temp = a[j];
+				a[j] = a[j+1];
+				a[j+1] = temp;
+			}
+		}
 }
 
 void pantalla(int a[], int n){
