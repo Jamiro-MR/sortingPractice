61f1a55d8255775866e31b34e9bf7ffaf15cfd61
JamiroMR <bgutierrezJY@hotmail.com>
2022-10-07 09:14:55 -0500

    Simple sorting practice(ordering numbers)

---
+++ sorting/insertion_sort.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+void insertionSort(int a[], int n){
+	for(int i=1;i<n;i++){
+		int key = a[i];
+		int j = i-1;
+		while(key<a[j] && j >=0){
+			a[j+1] = a[j];
+			--j;
+		}
+		a[j+1] = key;
+	}
+}
+void bubble_sort(int a[], int n){
+
+}
+
+void pantalla(int a[], int n){
+	for(int j=0; j<n-1; j++)
+		printf("%d, ", a[j]);
+	printf("%d\n", a[n-1]);
+}
+
+void main(){
+
+	int t=0;
+
+	printf("Cuantos numeros quiere permutar? ");
+	scanf("%d", &t);
+
+	int a[t];
+
+	for (int i=0;i<t;i++){
+		printf("Ingrese el valor %d:", i+1);
+		scanf("%d", &a[i]);
+	}
+	printf("\nValores desordenados: ");
+	pantalla(a,t);
+	insertionSort(a,t);
+	printf("\nValores ordenados: ");
+	pantalla(a,t);
+	printf("\n\n");
+
